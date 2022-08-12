e8c06966cbb39e8bde00281f1ca4bd9a4673182a
hzzz <hzzz0823@gmail.com>
2022-08-12 10:56:22 +0800

    https://spring.io/guides/gs/uploading-files/

--- src/main/java/com/hzzz/demo/DemoApplication.java
+++ src/main/java/com/hzzz/demo/DemoApplication.java
@@ -1,9 +1,12 @@
 package com.hzzz.demo;
 
+import com.hzzz.demo._1.uploadingfiles.storage.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/hzzz/demo/_1/uploadingfiles/storage/exception/StorageException.java
@@ -0,0 +1,12 @@
+package com.hzzz.demo._1.uploadingfiles.storage.exception;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
